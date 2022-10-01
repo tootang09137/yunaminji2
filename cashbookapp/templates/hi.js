f2ae3631b0c4301c3205a41f49306e213668e253
minji Lim <isk09137@naver.com>
2022-10-01 22:42:27 +0900

    윤아님 코드 3개 추가

---
+++ cashbookapp/templates/hi.js
@@ -0,0 +1,25 @@
+var slideWrapper = document.querySelector('.container');
+var slides = document.querySelectorAll('.item');
+var totalSlides = slides.length; 
+
+
+var sliderWidth = slideWrapper.clientWidth; 
+var slideIndex = 0;
+var slider = document.querySelector('.slider');
+
+slider.style.width = sliderWidth * totalSlides + 'px';
+
+showSlides()
+
+function showSlides() {
+    for(var i=0;i<slides.length;i++){
+        slider.style.left = -(sliderWidth * slideIndex) + 'px';    
+    }
+    slideIndex++;
+    if (slideIndex === totalSlides) {
+        slideIndex = 0;
+    }
+    setTimeout(showSlides, 1000); 
+}
+
+// 스크롤 효과 이녀석.. 넣고 싶었다.
