406e6a44badbbd6dbc4758dec7c35b581dd3ab66
profitah <qwer120542@gmail.com>
2022-11-13 21:58:28 +0900

    다크모드 추가 성공

--- cashbookapp/static/js/heart.js
+++ cashbookapp/static/js/heart.js
@@ -2,12 +2,12 @@ const heart = document.querySelector(".heart");
 const animatioHeart = document.querySelector(".animation-heart");
 
 heart.addEventListener('click', () => {
-    animatioHeart.classList.add('animation-heart');
+    animatioHeart.classList.add('animation');
     heart.classList.add('fill-color');
 });
 
 animatioHeart.addEventListener('click', () =>
 {
-    animatioHeart.classList.remove('animation-heart');
-    heart.classList.remove('heart');
+    animatioHeart.classList.remove('animation');
+    heart.classList.remove('fill-color');
 });
